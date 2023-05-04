3a61b40731456067aae9dd85f8b4fc591ba168ce
Andrey-EOFF <113987678+Andrey-EOFF@users.noreply.github.com>
2023-05-04 15:09:02 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,68 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  margin: 0 auto;
+  max-width: 600px;
+  padding: 20px;
+`;
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return total === 0 ? 0 : Math.round((good / total) * 100);
+  };
+
+  handleLeaveFeedback = option => {
+    this.setState(prevState => ({ [option]: prevState[option] + 1 }));
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+
+    return (
+      <StyledApp>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={totalFeedback}
+              positivePercentage={positivePercentage}
+            />
+          )}
+        </Section>
+      </StyledApp>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const FeedbackButton = styled.button`
+  font-size: 16px;
+  padding: 8px 16px;
+  margin-right: 8px;
+`;
+
+const FeedbackOptionsContainer = styled.div`
+  display: flex;
+  margin-top: 8px;
+`;
+
+export { FeedbackButton, FeedbackOptionsContainer };
